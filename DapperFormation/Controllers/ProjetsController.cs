4714dee0e81b0103f641936cc806810958f8f522
MarcosDevTi <marcos.dev.ti@gmail.com>
2021-04-02 04:00:46 -0400

    ajusts

--- DapperFormation/Controllers/ProjetsController.cs
+++ DapperFormation/Controllers/ProjetsController.cs
@@ -28,78 +28,73 @@ namespace DapperFormation.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var projetsDapper = await ObtenirProjets();
-
-            var projets = _efContext.ProjetsIncludeAll().OrderBy(p => p.Nom);
+            //var projets = _efContext.ProjetsIncludeAll().OrderBy(p => p.Nom);
+            var projetsDapper = await ObtenirProjects();
             return View(projetsDapper);
         }
 
-        public async Task<IEnumerable<Projet>> ObtenirProjets()
+        public async Task<IEnumerable<Projet>> ObtenirProjects()
         {
-            var sql = @"select 
-                        -- projet
-                        proj.id, proj.nom,
-                        -- déclaration
-                        decl.id, decl.nom, decl.id_projet ProjetId,
-                        -- pièce jointe
-                        pj.id, pj.titre, pj.id_document DocumentId, pj.id_attestation AttestationId, pj.id_declaration DeclarationId,
-                        -- document
-                        doc.id, doc.nom_fichier NomFichier,
-                        -- attestation
-                        attest.id, attest.nom, attest.id_professionnel ProfessionnelId,
-                        -- professionnel
-                        prof.id, prof.nom
-                        from oe_projet proj
-                        inner join oe_declaration decl on decl.id_projet = proj.id
-                        inner join oe_piece_jointe pj on pj.id_declaration = decl.id
-                        inner join oe_document doc on doc.id = pj.id_document
-                        inner join oe_attestation attest on attest.id = pj.id_attestation
-                        inner join oe_professionnel prof on prof.id = attest.id_professionnel
-                        order by proj.nom";
+            var sql = @"
+            select 
+            -- projet
+            proj.id, proj.nom,
+            -- déclaration
+            decl.id, decl.nom, decl.id_projet ProjetId,
+            -- pièce jointe
+            pj.id, pj.titre, pj.id_document DocumentId, pj.id_attestation AttestationId,pj.id_declaration DeclarationId,
+            -- Document
+            doc.id, doc.nom_fichier NomFichier,
+            -- Attestation
+            attest.id, attest.nom, attest.id_professionnel ProfessionnelId,
+            -- Professionnel
+            prof.id, prof.nom
+            from oe_projet proj
+            inner join oe_declaration decl on decl.id_projet = proj.id
+            inner join oe_piece_jointe pj on pj.id_declaration = decl.id
+            inner join oe_document doc on doc.id = pj.id_document
+            inner join oe_attestation attest on attest.id = pj.id_attestation
+            inner join oe_professionnel prof on prof.id = attest.id_professionnel";
 
-            var projDect = new Dictionary<int, Projet>();
-            var declDict = new Dictionary<int, Declaration>();
-            var blequerDeclaration = false;
-            var pjDict = new Dictionary<int, PieceJointe>();
+            var dictProjet = new Dictionary<int, Projet>();
+            var dictDecl = new Dictionary<int, Declaration>();
+            var dictPj = new Dictionary<int, PieceJointe>();
 
-            var resultat = await _dapperConnection.QueryAsync<Projet, Declaration, PieceJointe, Document, Attestation, Professionnel, Projet>(sql,
-                  (p, d, pj, doc, attest, prof) =>
-                  {
-                      if (!projDect.TryGetValue(p.Id, out Projet projet))
-                          projDect.Add(p.Id, projet = p);
+            var projets = await _dapperConnection.QueryAsync<Projet, Declaration, PieceJointe, Document, Attestation, Professionnel, Projet>(sql,
+            (p, d, pj, doc, attest, prof) =>
+            {
+                if (!dictProjet.TryGetValue(p.Id, out Projet projet))
+                    dictProjet.Add(p.Id, projet = p);
 
-                      if (!declDict.TryGetValue(d.Id, out Declaration declaration))
-                      {
-                          declDict.Add(d.Id, declaration = d);
-                          blequerDeclaration = true;
-                      }
+                var estDeclarationDejaAjoute = dictDecl.TryGetValue(d.Id, out Declaration declaration);
+                if (!estDeclarationDejaAjoute)
+                    dictDecl.Add(d.Id, declaration = d);
 
-                      if (!pjDict.TryGetValue(pj.Id, out PieceJointe pieceJointe))
-                      {
-                          pjDict.Add(pj.Id, pieceJointe = pj);
-                          pieceJointe.Document = doc;
-                          attest.Professionnel = prof;
-                          pieceJointe.Attestation = attest;
-                      }
+                if (!dictPj.TryGetValue(pj.Id, out PieceJointe pieceJointe))
+                {
+                     dictPj.Add(pj.Id, pieceJointe = pj);
+                     pieceJointe.Document = doc;
+                     attest.Professionnel = prof;
+                     pieceJointe.Attestation = attest;
+                }
 
-                      if (declaration.PiecesJointes is null)
-                          declaration.PiecesJointes = new List<PieceJointe>();
-                      ((List<PieceJointe>)declaration.PiecesJointes).Add(pieceJointe);
+                if (declaration.PiecesJointes is null)
+                    declaration.PiecesJointes = new List<PieceJointe>();
+                ((IList<PieceJointe>)declaration.PiecesJointes).Add(pieceJointe);
 
-                      if (projet.Declarations is null)
-                          projet.Declarations = new List<Declaration>();
-                      if (blequerDeclaration)
-                      {
-                          ((List<Declaration>)projet.Declarations).Add(declaration);
-                          blequerDeclaration = false;
-                      }
+                if (projet.Declarations is null)
+                    projet.Declarations = new List<Declaration>();
 
-                      return projet;
-                  }
+                if (!estDeclarationDejaAjoute)
+                {
+                    ((IList<Declaration>)projet.Declarations).Add(declaration);
+                }
+
+                return p;
+            }
             );
 
-            var res = projDect.Values;
-            return res;
+            return dictProjet.Values;
         }
     }
 }
